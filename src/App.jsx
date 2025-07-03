1f029b944157377030bf38426704c34a81d25fcc
muuo <anthonymuuo92@gmail.com>
2025-07-03 19:09:46 +0300

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>react query practice from react query documentation</h1>
+    </div>
+  );
+};
+
+export default App;
