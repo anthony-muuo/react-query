ae6a8d34a381e35e3ed413e674a5552df915ce73
muuo <anthonymuuo92@gmail.com>
2025-07-03 21:18:35 +0300

    installed the react-query, set up queryclient and finally created a query to return book title

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 
-const App = () => {
+const queryClient = new QueryClient();
+
+async function getBooksTitle() {
+  const response = await Promise.resolve("The Hobbit");
+  return response;
+}
+function Book() {
+  const { data } = useQuery({
+    queryKey: ["bookTitle"],
+    queryFn: getBooksTitle,
+  });
   return (
     <div>
-      <h1>react query practice from react query documentation</h1>
+      <header className="app-header">
+        <h1>
+          <span>Query Library</span>
+        </h1>
+      </header>
+      <main>
+        <h2 className="book-title">{data}</h2>
+      </main>
     </div>
+  );
+}
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Book />
+    </QueryClientProvider>
   );
 };
 
